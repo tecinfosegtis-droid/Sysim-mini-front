36a97ed7d43eaf95ef468387f2d3a4b5cc7cec40
tecinfosegtis-droid <tecinfosegtis@gmail.com>
2025-09-03 21:52:22 -0300

    Update page.js

--- app/os/page.js
+++ app/os/page.js
@@ -1,5 +1,12 @@
 'use client'
-export const metadata = { title: 'Sysim Mini – OS' }
+export const metadata = {
+  title: 'Sysim Mini – OS',
+};
+
+export default function OSLayout({ children }) {
+  return children;
+}
+
 
 import { useEffect, useState } from 'react'
 import { supabase } from '../lib/supabaseClient'
